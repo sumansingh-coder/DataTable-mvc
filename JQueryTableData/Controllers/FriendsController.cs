df109d5c4b4d145181c958efb1e832c3b2d7ca60
sumansingh-coder <suman159singh@gmail.com>
2023-11-24 18:47:06 +0530

    initial commit

---
+++ JQueryTableData/Controllers/FriendsController.cs
@@ -0,0 +1,27 @@
+﻿using JQueryTableData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JQueryTableData.Controllers
+{
+    public class FriendsController : Controller
+    {
+        // GET: Friends
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetList() 
+        {
+            using (DBModels db = new DBModels())
+            {
+                var frdList = db.Friends.ToList<Friend>();
+                return Json(new { data=frdList }, JsonRequestBehavior.AllowGet);
+            }   
+        }
+    }
+}
